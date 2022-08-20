cb02875a8daabec8e2b308505d39e2990ff0037d
tingamapuro04 <alponcemobutu100@gmail.com>
2022-08-20 07:54:23 +0300

    max char solution

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,5 +5,16 @@ function maxChar(str) {
   let maxchar = ""
   let maxCount = 0;
 
-  
+  // Fill out the character object
+  for (let i = 0; i < str.length; i ++) {
+    const char = str[i];
+    maxcharobj[char] = maxcharobj[char] + 1 || 1;
+    if (maxcharobj[char] > maxCount) {
+      maxChar = char;
+      maxCount = maxcharobj[char]
+    }
+  }
+  return maxChar;
 }
+
+console.log(maxChar('1337'));
