a0ca33d35b7bac8e5b29c633e9f1df1e544a471e
tingamapuro04 <alponcemobutu100@gmail.com>
2022-08-22 09:36:24 +0300

    Add second solution after refactor of code

--- Capitalize/index.js
+++ Capitalize/index.js
@@ -1,7 +1,5 @@
 const capitalise = (str) => {
-  const splitted = str.split(" ");
-  const capped = splitted.map(item => item.charAt(0).toUpperCase() + item.slice(1,));
-  return capped.join(" ");
+  return str.split(" ").map(item => item.charAt(0).toUpperCase() + item.slice(1,)).join(" ");
 }
 
 console.log(capitalise('I am here today'))
