46e289643682242822223fab6f0ef66129b530ed
tingamapuro04 <alponcemobutu100@gmail.com>
2022-08-29 21:57:38 +0300

    refactor

--- steps/index.js
+++ steps/index.js
@@ -2,7 +2,7 @@ console.log('#'.repeat(4));
 
 function Steps (n) {
   for (let i = 1; i <= n; i++) {
-    console.log(' '.repeat(n-i).concat('#'.repeat(i)))
+    console.log('#'.repeat(i).concat(' '.repeat(n-i)))
   }
 }
 Steps(6)
