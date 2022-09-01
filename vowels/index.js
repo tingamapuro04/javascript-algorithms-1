76a7ea0d7bab19b28734a0ebe4c85a9ee968ea15
tingamapuro04 <alponcemobutu100@gmail.com>
2022-09-01 21:37:52 +0300

    Add folder

--- vowels/index.js
+++ vowels/index.js
@@ -2,11 +2,11 @@ function vowels(str) {
   // const maneno = ['a', 'e', 'i', 'o', 'u']
   let count = 0;
   for (let i = 0; i < str.length; i++) {
-    if ('aeiou'.includes(str[i])) {
+    if ('aeiou'.includes(str[i].toLowerCase())) {
       count++
     }
   }
   return count
 }
 
-console.log(vowels('whyai'));
+console.log(vowels('whyAI'));
